32f6a4c2cd3bebf3ce4ea24546a8c59417d91c4b
Blesson484 <62725085+Blesson484@users.noreply.github.com>
2021-02-24 13:22:27 +0530

    Update new.java

--- new.java
+++ new.java
@@ -3,6 +3,8 @@ public class HelloGuys {
            public static void main(String[] args) {
                          System.out.println("hey");
                          System.out.println("how are you");
+                         System.out.println("ok");
+
            
          }
   
