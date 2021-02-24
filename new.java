5a211478e7b45418481ba661fb41ea2f9770c17a
Blesson484 <62725085+Blesson484@users.noreply.github.com>
2021-02-24 13:09:58 +0530

    Create new.java

---
+++ new.java
@@ -0,0 +1,10 @@
+public class HelloGuys (
+  
+  
+         public ststic void main(String[] args) {
+                         System.out.println("hey");
+                         System.out.println("how are you");
+           
+         }
+  
+  }
